863c87ec3fc1bc3cf6d1a7597a140a2887366e79
Timothy Chen <t.chen0120241@arts.ac.uk>
2025-03-02 02:10:52 +0000

    Quotable API for random quotes + CSS Styling + Credits

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 const quotes = ["quote1", "quote2", "quote3"];
 const button = document.querySelector('button');
-const body = document.querySelector('body');
+const quoteContainer = document.querySelector('.container');
 
-const quoteElement = document.createElement('h2');
-document.body.appendChild(quoteElement);
+const quoteText = document.createElement('h2');
+const quoteAuthor = document.createElement('h3');
+
+quoteText.classList.add("quote");
+quoteAuthor.classList.add("author");
+
+quoteContainer.appendChild(quoteText);
+quoteContainer.appendChild(quoteAuthor);
+
+
 
 button.addEventListener("click", generateQuote);
 
-function generateQuote() {
-    const randomIndex = Math.floor(Math.random()*quotes.length);
-    quoteElement.textContent = quotes[randomIndex];
+async function generateQuote() {
+
+    // Remove animation class to reset effect
+    quoteText.classList.remove("show");
+    quoteAuthor.classList.remove("show");
+
+    // Fetch quote data from the API
+    const response = await fetch('http://api.quotable.io/random');
+    const data = await response.json();
+
+    // Update quote content
+    quoteText.textContent = '"' + data.content + '"';
+    quoteAuthor.textContent = "by " + data.author;
+
+
+    // Add animation class to apply effect
+    setTimeout(() => {
+        quoteText.classList.add("show");
+        quoteAuthor.classList.add("show");
+        quoteContainer.classList.add("show");
+    }, 10);
+
+
+
 }
+
 
